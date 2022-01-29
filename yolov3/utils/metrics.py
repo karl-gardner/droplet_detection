5d22a5388069f2d680e393b51186d67368d51186
karl-gardner <karl.gardner@ttu.edu>
2022-01-29 13:36:45 -0600

    Update metrics.py

--- yolov3/utils/metrics.py
+++ yolov3/utils/metrics.py
@@ -298,23 +298,28 @@ def wh_iou(wh1, wh2):
 def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
     # Precision-recall curve
     fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
+    # inset axiss....
+    axins = ax.inset_axes([0.5, 0.5, 0.47, 0.47])
+   
     py = np.stack(py, axis=1)
 
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
         for i, y in enumerate(py.T):
             ax.plot(px, y, linewidth=1, label=f'{names[i]} {ap[i, 0]:.3f}')  # plot(recall, precision)
+            axins.plot(px, y, linewidth=1, label=f'{names[i]} {ap[i, 0]:.3f}')  # plot(recall, precision)
     else:
         ax.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
+        axins.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
+        
 
     ax.plot(px, py.mean(1), linewidth=3, color='blue', label='all classes %.3f mAP@0.5' % ap[:, 0].mean())
+    axins.plot(px, py.mean(1), linewidth=3, color='blue', label='all classes %.3f mAP@0.5' % ap[:, 0].mean())
     ax.set_xlabel('Recall', fontsize=30)
     ax.set_ylabel('Precision', fontsize=30)
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.tick_params(labelsize=15)
-    # inset axes....
-    axins = ax.inset_axes([0.5, 0.5, 0.47, 0.47])
-    axins.imshow(Z2, extent=extent, origin="lower")
+    
     # sub region of the original image
     x1, x2, y1, y2 = .6, .7, .7, .8
     axins.set_xlim(x1, x2)
