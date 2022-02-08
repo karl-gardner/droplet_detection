d0234eca4be567174486753303b429956a6831c6
karl-gardner <karl.gardner@ttu.edu>
2022-02-08 12:31:12 -0600

    Update metrics.py

--- yolov3/utils/metrics.py
+++ yolov3/utils/metrics.py
@@ -326,7 +326,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', save_leg = "path", names=
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.tick_params(labelsize=21)
-    fig.suptitle("YOLOv3",fontsize=20,y=1)
+    fig.suptitle("YOLOv3",fontsize=24,y=1.1)
     
     # sub region of the original image
     x1, x2, y1, y2 = .8, 1, .5, 1
