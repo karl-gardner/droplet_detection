1bf9e803d62711cfb20b011223b108bf6334a300
karl-gardner <karl.gardner@ttu.edu>
2022-02-08 12:37:07 -0600

    Update metrics.py

--- yolov3/utils/metrics.py
+++ yolov3/utils/metrics.py
@@ -297,7 +297,7 @@ def wh_iou(wh1, wh2):
 
 def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', save_leg = "path", names=()):
     # Precision-recall curve
-    fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
+    fig, ax = plt.subplots(1, 1, figsize=(9, 7), tight_layout=True)
     
     # save legend externally
     legendFig = plt.figure("Legend plot", figsize=[14,2])
@@ -326,7 +326,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', save_leg = "path", names=
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     ax.tick_params(labelsize=21)
-    fig.suptitle("YOLOv3",fontsize=24,y=1.01)
+    fig.suptitle("YOLOv3",fontsize=35,y=1.01)
     
     # sub region of the original image
     x1, x2, y1, y2 = .8, 1, .5, 1
