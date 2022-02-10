85a91083e2c582590d6cae5cd53e84b9b092eed8
karl-gardner <karl.gardner@ttu.edu>
2022-02-10 02:48:36 -0600

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -78,7 +78,7 @@ def box_label(box, label='', color=(128, 128, 128), txt_color=(255, 255, 255)):
       
 def xywhn2xyxy(x, w=640, h=640, padw=0, padh=0):
     # Convert nx4 boxes from [x, y, w, h] normalized to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
     y[:, 0] = w * (x[:, 0] - x[:, 2] / 2) + padw  # top left x
     y[:, 1] = h * (x[:, 1] - x[:, 3] / 2) + padh  # top left y
     y[:, 2] = w * (x[:, 0] + x[:, 2] / 2) + padw  # bottom right x
