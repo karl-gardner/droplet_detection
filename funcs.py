f9f53e642cf5823e7940dfcd6aab6ce5ca5c7fc9
karl-gardner <karl.gardner@ttu.edu>
2022-02-10 13:27:14 -0600

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -74,7 +74,7 @@ def box_label(image, box, label='', color=(128, 128, 128), txt_color=(255, 255, 
       outside = p1[1] - h - 3 >= 0  # label fits outside box
       p2 = p1[0] + w, p1[1] - h - 3 if outside else p1[1] + h + 3
       cv2.rectangle(image, p1, p2, color, -1, cv2.LINE_AA)  # filled
-      cv2.putText(image, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, 1 / 3, txt_color,
+      cv2.putText(image, label, (p1[0], p1[1] - 2 if outside else p1[1] + h + 2), 0, 5, txt_color,
                   thickness=tf, lineType=cv2.LINE_AA)
   return image
       
