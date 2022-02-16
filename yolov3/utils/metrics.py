a9609a24d26d263f3cb68f5a2cca0aef52bb55fb
karl-gardner <karl.gardner@ttu.edu>
2022-02-16 13:29:32 -0600

    Update metrics.py

--- yolov3/utils/metrics.py
+++ yolov3/utils/metrics.py
@@ -313,7 +313,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', save_leg = "path", names=
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
         for i, y in enumerate(py.T):
             ax.plot(px, y, linewidth=1, color=colors[i])  # plot(recall, precision))
-            patches.append(mpatches.Patch(color=colors[i])
+            patches.append(mpatches.Patch(color=colors[i]))
             text.append(names[i])
             axins.plot(px, y, linewidth=2)
     else:
@@ -322,7 +322,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', save_leg = "path", names=
         
 
     ax.plot(px, py.mean(1), linewidth=3, color='blue')
-    patches.append(mpatches.Patch(color='blue')
+    patches.append(mpatches.Patch(color='blue'))
     text.append('all classes @ IOU 0.5')
     axins.plot(px, py.mean(1), linewidth=3, color='blue')
     ax.set_xlabel('Recall', fontsize=30)
