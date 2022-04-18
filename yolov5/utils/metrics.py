9acd2c094ea70d35785685b8678c85c03f60ee5d
karl-gardner <karl.gardner@ttu.edu>
2022-04-17 19:50:24 -0500

    Update metrics.py

--- yolov5/utils/metrics.py
+++ yolov5/utils/metrics.py
@@ -117,7 +117,7 @@ def compute_ap(recall, precision):
 class ConfusionMatrix:
     # Updated version of https://github.com/kaanakan/object_detection_confusion_matrix
     def __init__(self, nc, conf=0.25, iou_thres=0.45):
-        self.matrix = np.zeros((nc + 1, nc + 1))
+        self.matrix = np.zeros((nc, nc))
         self.nc = nc  # number of classes
         self.conf = conf
         self.iou_thres = iou_thres
