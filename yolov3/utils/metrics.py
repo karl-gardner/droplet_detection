0c2f7fb3814346fff021bf9f43acc4e8a404d93c
karl-gardner <karl.gardner@ttu.edu>
2022-04-17 19:14:50 -0500

    Update metrics.py

--- yolov3/utils/metrics.py
+++ yolov3/utils/metrics.py
@@ -182,7 +182,7 @@ class ConfusionMatrix:
 #                            yticklabels=names + ['background FN'] if labels else "auto").set_facecolor((1, 1, 1))
                 sn.heatmap(array, annot=self.nc < 30, annot_kws={"size": 20}, cmap='Blues', fmt='d', square=True,
                            xticklabels=names,yticklabels=names).set_facecolor((1, 1, 1))
-            fig.axes[0].set_title('Predicted',fontsize=45)
+            fig.axes[0].set_title('YOLOv3',fontsize=45)
             fig.axes[0].set_xlabel('True', fontsize=35)
             fig.axes[0].set_ylabel('Predicted',fontsize=35)
             fig.savefig(Path(save_dir) / 'confusion_matrix.tif', dpi=500, bbox_inches='tight')
