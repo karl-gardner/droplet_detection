2dbba9aaaef8cc910352d12d759a86078ff7dd72
karl-gardner <karl.gardner@ttu.edu>
2022-05-21 16:37:09 -0500

    Update metrics.py

--- yolov5/utils/metrics.py
+++ yolov5/utils/metrics.py
@@ -202,98 +202,6 @@ class ConfusionMatrix:
         for i in range(self.nc + 1):
             print(' '.join(map(str, self.matrix[i])))
 
-# class ConfusionMatrix:
-#     # Updated version of https://github.com/kaanakan/object_detection_confusion_matrix
-#     def __init__(self, nc, conf=0.25, iou_thres=0.45):
-#         self.matrix = np.zeros((nc + 1, nc + 1))
-#         self.nc = nc  # number of classes
-#         self.conf = conf
-#         self.iou_thres = iou_thres
-
-#     def process_batch(self, detections, labels):
-#         """
-#         Return intersection-over-union (Jaccard index) of boxes.
-#         Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
-#         Arguments:
-#             detections (Array[N, 6]), x1, y1, x2, y2, conf, class
-#             labels (Array[M, 5]), class, x1, y1, x2, y2
-#         Returns:
-#             None, updates confusion matrix accordingly
-#         """
-#         detections = detections[detections[:, 4] > self.conf]
-#         gt_classes = labels[:, 0].int()
-#         detection_classes = detections[:, 5].int()
-#         iou = box_iou(labels[:, 1:], detections[:, :4])
-
-#         x = torch.where(iou > self.iou_thres)
-#         if x[0].shape[0]:
-#             matches = torch.cat((torch.stack(x, 1), iou[x[0], x[1]][:, None]), 1).cpu().numpy()
-#             if x[0].shape[0] > 1:
-#                 matches = matches[matches[:, 2].argsort()[::-1]]
-#                 matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
-#                 matches = matches[matches[:, 2].argsort()[::-1]]
-#                 matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
-#         else:
-#             matches = np.zeros((0, 3))
-
-#         n = matches.shape[0] > 0
-#         m0, m1, _ = matches.transpose().astype(np.int16)
-#         for i, gc in enumerate(gt_classes):
-#             j = m0 == i
-#             if n and sum(j) == 1:
-#                 self.matrix[detection_classes[m1[j]], gc] += 1  # correct
-#             else:
-#                 self.matrix[self.nc, gc] += 1  # background FP
-
-#         if n:
-#             for i, dc in enumerate(detection_classes):
-#                 if not any(m1 == i):
-#                     self.matrix[dc, self.nc] += 1  # background FN
-
-#     def matrix(self):
-#         return self.matrix
-
-#     def tp_fp(self):
-#         tp = self.matrix.diagonal()  # true positives
-#         fp = self.matrix.sum(1) - tp  # false positives
-#         # fn = self.matrix.sum(0) - tp  # false negatives (missed detections)
-#         return tp[:-1], fp[:-1]  # remove background class
-
-#     def plot(self, normalize=True, save_dir='', names=()):
-#         try:
-#             import seaborn as sn
-
-#             array = self.matrix / ((self.matrix.sum(0).reshape(1, -1) + 1E-9) if normalize else 1)  # normalize columns
-#             array[array < 0.005] = np.nan  # don't annotate (would appear as 0.00)
-
-#             fig = plt.figure(figsize=(12, 9), tight_layout=True)
-#             nc, nn = self.nc, len(names)  # number of classes, names
-#             sn.set(font_scale=1.0 if nc < 50 else 0.8)  # for label size
-#             labels = (0 < nn < 99) and (nn == nc)  # apply names to ticklabels
-#             with warnings.catch_warnings():
-#                 warnings.simplefilter('ignore')  # suppress empty matrix RuntimeWarning: All-NaN slice encountered
-#                 sn.heatmap(array,
-#                            annot=nc < 30,
-#                            annot_kws={
-#                                "size": 8},
-#                            cmap='Blues',
-#                            fmt='.2f',
-#                            square=True,
-#                            vmin=0.0,
-#                            xticklabels=names + ['background FP'] if labels else "auto",
-#                            yticklabels=names + ['background FN'] if labels else "auto").set_facecolor((1, 1, 1))
-#             fig.axes[0].set_xlabel('True')
-#             fig.axes[0].set_ylabel('Predicted')
-#             fig.savefig(Path(save_dir) / 'confusion_matrix.png', dpi=250)
-#             plt.close()
-#         except Exception as e:
-#             print(f'WARNING: ConfusionMatrix plot failure: {e}')
-
-#     def print(self):
-#         for i in range(self.nc + 1):
-#             print(' '.join(map(str, self.matrix[i])))
-
-
 def bbox_iou(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False, eps=1e-7):
     # Returns Intersection over Union (IoU) of box1(1,4) to box2(n,4)
 
@@ -392,27 +300,49 @@ def wh_iou(wh1, wh2):
 
 
 # Plots ----------------------------------------------------------------------------------------------------------------
-
+# karl-gardner changes in plot_pr_curve --------------------------------------------------------------------------------
 
 def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
     # Precision-recall curve
-    fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
+    fig, ax = plt.subplots(1, 1, figsize=(15, 6), tight_layout=True)
+    
+    # save legend externally
+    legendFig = plt.figure("Legend plot", figsize=[14,2])
+    
+    # inset axis....
+    axins = ax.inset_axes([0.2, 0.05, 0.3, 0.7])
+   
     py = np.stack(py, axis=1)
 
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
         for i, y in enumerate(py.T):
             ax.plot(px, y, linewidth=1, label=f'{names[i]} {ap[i, 0]:.3f}')  # plot(recall, precision)
+            axins.plot(px, y, linewidth=2)
     else:
         ax.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
-
-    ax.plot(px, py.mean(1), linewidth=3, color='blue', label='all classes %.3f mAP@0.5' % ap[:, 0].mean())
-    ax.set_xlabel('Recall')
-    ax.set_ylabel('Precision')
+        axins.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
+    
+    ax.plot(px, py.mean(1), linewidth=3, color='blue')
+    axins.plot(px, py.mean(1), linewidth=3, color='blue')
+    ax.set_xlabel('Recall', fontsize=30)
+    ax.set_ylabel('Precision', fontsize=30)
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
-    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-    fig.savefig(Path(save_dir), dpi=250)
+    ax.tick_params(labelsize=26)
+    
+    # sub region of the original image
+    x1, x2, y1, y2 = .8, 1, .5, 1
+    axins.set_xlim(x1, x2)
+    axins.set_ylim(y1, y2)
+    axins.set_xticks([])
+    axins.set_yticks([])
+    ax.indicate_inset_zoom(axins, edgecolor="black")
+    ax.set_title("YOLOv5",fontsize=38)
+    
+#     plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+    fig.savefig(Path(save_dir), dpi=500)
     plt.close()
+
 
 
 def plot_mc_curve(px, py, save_dir='mc_curve.png', names=(), xlabel='Confidence', ylabel='Metric'):
