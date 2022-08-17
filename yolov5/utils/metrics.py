912682c7f2e503f9454908ac30824fce95cdaf30
karl-gardner <karl.gardner@ttu.edu>
2022-08-17 13:13:01 -0500

    Update metrics.py

--- yolov5/utils/metrics.py
+++ yolov5/utils/metrics.py
@@ -315,7 +315,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
         for i, y in enumerate(py.T):
             ax.plot(px, y, linewidth=1, label=f'{names[i]} {ap[i, 0]:.3f}')  # plot(recall, precision)
-            ax.plot(px, y, linewidth=1, label='single class cell @ IOU 0.5)  # plot(recall, precision)
+            ax.plot(px, y, linewidth=1, label='single class cell @ IOU 0.5')  # plot(recall, precision)
 #             axins.plot(px, y, linewidth=2)
     else:
         ax.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
